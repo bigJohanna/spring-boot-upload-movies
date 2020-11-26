d3d6c9d93d6cf6cc961bb9975348172e38960e5c
Johanna <svallingson@gmail.com>
2020-11-26 20:46:43 +0100

    first commit

---
+++ src/test/java/se/bigjohanna/uploadmovies/uploadmovies/UploadmoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package se.bigjohanna.uploadmovies.uploadmovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadmoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
